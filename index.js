f15d5d41b4ad59d2c6d674b9a9f7ae7c9db1b32a
Powerman-code <64640573+Powerman-code@users.noreply.github.com>
2021-02-20 00:59:22 +0200

    init

---
+++ index.js
@@ -0,0 +1,22 @@
+import fs from 'fs';
+import path from 'path';
+const contactsPath = path.join(__filename, 'db', contacts.json);
+console.log(contactsPath);
+// const fs = require('fs');
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
